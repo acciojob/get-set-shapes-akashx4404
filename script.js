ccd3fca7391242c897340c2415ac508c9b4df059
Akash Kumar <akash29370@gmail.com>
2025-12-21 15:19:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// complete this code
 class Rectangle {
   constructor(width, height) {
     this._width = width;
@@ -21,10 +20,14 @@ class Rectangle {
 class Square extends Rectangle {
   constructor(side) {
     super(side, side);
-    this._side = side;
   }
 
   getPerimeter() {
-    return 4 * this._side;
+    return 4 * this.width;
   }
 }
+
+// Cypress ke liye important
+window.Rectangle = Rectangle;
+window.Square = Square;
+
